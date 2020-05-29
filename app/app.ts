0bf46215a09ab58a823a8a39fb60e6f9fa306846
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-29 13:14:09 -0600

    Remove 'strict' option. Add logPlayer fn

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,16 @@
 function startGame() {
     //start a new game
+
+    const playerName: string = 'Audrey';
+    logPlayer(playerName);
+
     var messagesElement = document.getElementById('messages');
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
+
+function logPlayer(name) {
+    console.log(`New game starting for player: ${name}`);
+}
+
 //exclamation mark asserts that the object is non null
 document.getElementById('startGame')!.addEventListener('click', startGame);
