bdc5c66f9cc1af9864ac21e04a257f1b3765d986
Gerax Sotelo <gsotelo23@gmail.com>
2020-06-02 14:53:52 -0600

    give variable a fn type to assign it any fn of same type

--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,7 @@ function startGame() {
     logPlayer(playerName);
 
     postScore(100, playerName);
+    postScore(-5, playerName);
 
     var messagesElement = document.getElementById('messages');
     messagesElement!.innerText = 'Welcome to MultiMath! Started new game...';
@@ -30,9 +31,29 @@ function getInputValue(elementID: string): string | undefined {
 
 //the question mark character flags the param as optional
 function postScore(score: number, playerName?: string): void {
+    //to give a var a fn type, add a colon after the var name, then put expected fn params & their types in parenthesis followed by an arrow & expected fn return type.
+    //this 'logger' var may now be assigned any fn that takes a single string param & returns void.
+    let logger: (value: string) => void;
+
+    if (score < 0) {
+        logger = logError;
+    }
+    else {
+        logger = logMessage;
+    }
+
     const scoreElement: HTMLElement | null = document.getElementById('postedScores');
     scoreElement!.innerText = `${score} - ${playerName}`;
+
+    logger(`Score: ${score}`);
 }
 
 //exclamation mark asserts that the object is non null
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+//compiler infers return type is void
+const logMessage = (message: string) => console.log(message);
+
+function logError(err: string): void {
+    console.error(err);
+}
