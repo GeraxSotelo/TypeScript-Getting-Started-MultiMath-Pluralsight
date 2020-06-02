5a9f86e67af6cd03fa5581f900bf892c25cc0151
Gerax Sotelo <gsotelo23@gmail.com>
2020-06-02 13:07:10 -0600

    add functions to get name value & post score

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,37 @@
 function startGame() {
     //start a new game
 
-    let playerName: string = 'Audrey';
+    let playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName);
 
+    postScore(100, playerName);
+
     var messagesElement = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    messagesElement!.innerText = 'Welcome to MultiMath! Started new game...';
 }
 
-function logPlayer(name: string): void {
+//using a default initialized param makes that param optional
+function logPlayer(name: string = 'MultiMath Player'): void {
     console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID: string): string | undefined {
+    //this function normally returns a value of the type HTMLElement
+    //use type assertion <HTMLInputElement> to treat the element it finds as the more specific HTMLInputElement type.
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+    if (inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+
+//the question mark character flags the param as optional
+function postScore(score: number, playerName?: string): void {
+    const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 //exclamation mark asserts that the object is non null
